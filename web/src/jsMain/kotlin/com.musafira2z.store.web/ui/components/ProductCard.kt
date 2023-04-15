a160bb066bb2946ba809cc540aca0822b575d56f
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2023-04-16 01:55:47 +0600

    mobile responsive option added

--- web/src/jsMain/kotlin/com.musafira2z.store.web/ui/components/ProductCard.kt
+++ web/src/jsMain/kotlin/com.musafira2z.store.web/ui/components/ProductCard.kt
@@ -166,7 +166,7 @@ fun Product(
     val productNode = product.node.productDetailsFragment
     Div(attrs = {
         classes(
-            "col-span-12",
+            "col-span-6",
             "sm:col-span-6",
             "md:col-span-6",
             "lg:col-span-3",
