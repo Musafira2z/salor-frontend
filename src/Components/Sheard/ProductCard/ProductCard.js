352b6064ab7dd3cfb13c1cf71c8b90421fcf520d
Your Name <you@example.com>
2023-07-22 19:37:12 +0600

    quantity increment decrement add in product card

--- src/Components/Sheard/ProductCard/ProductCard.js
+++ src/Components/Sheard/ProductCard/ProductCard.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { LanguageCodeEnum, useCheckoutAddProductLineMutation, /* useCheckoutByTokenQuery */ } from '../../../api';
+import { LanguageCodeEnum, useCheckoutAddProductLineMutation, useCheckoutByTokenQuery, useCheckoutLineUpdateMutation, useRemoveProductFromCheckoutMutation, /* useCheckoutByTokenQuery */ } from '../../../api';
 import { useLocalStorage } from 'react-use';
 import toast from 'react-hot-toast';
+import { BiPlusMedical } from 'react-icons/bi';
+import { ImMinus } from 'react-icons/im';
 
 const ProductCard = ({ data }) => {
 
@@ -10,6 +12,25 @@ const ProductCard = ({ data }) => {
     const { thumbnail, name, slug, variants } = data?.node;
 
     const [checkoutAddProductLine, { data: checkoutAddProduct, loading }] = useCheckoutAddProductLineMutation();
+    const [decrement, { data: decrementData, loading: decrementLoading }] = useCheckoutLineUpdateMutation();
+    const [RemoveProductFromCheckout] = useRemoveProductFromCheckoutMutation();
+
+
+
+    const { data: checkoutProducts } = useCheckoutByTokenQuery({
+        variables: {
+            checkoutToken: checkoutToken,
+            locale: LanguageCodeEnum.En,
+        }
+    })
+    const checkoutData = checkoutProducts?.checkout;
+
+
+
+
+    const items = checkoutData?.lines?.find(item => item?.variant?.id === variants?.[0]?.id);
+
+
 
 
 
@@ -26,29 +47,87 @@ const ProductCard = ({ data }) => {
 
 
 
+    const handleRemoveToCart = async () => {
+        await RemoveProductFromCheckout({
+            variables: {
+                checkoutToken: checkoutToken,
+                lineId: items?.id,
+                locale: LanguageCodeEnum.En,
+            }
+        })
+    }
+
+
+
+
+    const handleDecrementToCart = async () => {
+
+        if (items?.quantity > 1) {
+            await decrement({
+                variables: {
+                    token: checkoutToken,
+                    locale: LanguageCodeEnum.En,
+                    lines: [{
+                        quantity: items?.quantity - 1,
+                        variantId: items?.variant.id
+                    }]
+                }
+            })
+        } else {
+            await handleRemoveToCart();
+        }
+    }
+
+
+
+
+
+    useEffect(() => {
+        if (decrementLoading) {
+            toast.loading('Loading...', { id: 'checkout' })
+        }
+        if (decrementData?.checkoutLinesUpdate?.errors?.[0]?.message) {
+            toast.error(decrementData?.checkoutLinesUpdate?.errors?.[0]?.message, { id: 'checkout' })
+        }
+        if (decrementData?.checkoutLinesUpdate?.checkout?.id) {
+            toast.success(`Quantity: ${items?.quantity}`, { id: 'checkout' })
+        }
+
+    }, [
+        items?.quantity,
+        decrementData?.checkoutLinesUpdate?.errors,
+        decrementData?.checkoutLinesUpdate?.checkout?.id,
+        decrementLoading
+    ]);
+
+
+
+
 
     // error handling -------------------
 
     useEffect(() => {
         if (checkoutAddProduct?.checkoutLinesAdd?.checkout?.id) {
-            toast.success("Add to Cart success", {
-                id: 'addToCart'
+            toast.success(`Quantity: ${items?.quantity}`, {
+                id: 'checkout'
             })
 
         }
         if (loading) {
             toast.loading('Loading...', {
-                id: 'addToCart'
+                id: 'checkout'
             })
         }
         if (checkoutAddProduct?.checkoutLinesAdd?.errors?.[0]?.message) {
             toast.error(checkoutAddProduct?.checkoutLinesAdd?.errors?.[0]?.message, {
-                id: 'addToCart'
+                id: 'checkout'
             })
         }
     }, [
         checkoutAddProduct?.checkoutLinesAdd?.checkout?.id,
-        loading, checkoutAddProduct?.checkoutLinesAdd?.errors
+        checkoutAddProduct?.checkoutLinesAdd?.errors,
+        items?.quantity,
+        loading,
     ])
 
 
@@ -79,17 +158,43 @@ const ProductCard = ({ data }) => {
 
                     </div >
                 </Link>
-                
+
                 <div>
                     {data?.node?.variants?.[0]?.quantityAvailable === 0 ?
                         <button
                             disabled
                             className='  border-2 border-yellow-400 rounded-lg text-red-500  hover:text-slate-50 text-xs font-bold hover:duration-500 duration-500  py-3 px-1 md:px-6 w-full    hover:bg-gradient-to-r from-yellow-400 to-red-600' >Out Off Stock</button > :
 
-                        <button
+                        <div>
+                            {
+                                items ?
 
-                            onClick={() => handleAddToCart(variants?.[0]?.id)}
-                            className='  border-2 border-yellow-400 rounded-lg text-red-500  hover:text-slate-50 text-xs font-bold hover:duration-500 duration-500  py-3 px-1 md:px-6 w-full    hover:bg-gradient-to-r from-yellow-400 to-red-600' > Add to Cart</button >
+                                    <div className=' border-2 border-yellow-400 rounded-lg text-red-500  hover:text-slate-50 text-xs font-bold hover:duration-500 duration-500   md:px-6 w-full    hover:bg-gradient-to-r from-yellow-400 to-red-600 '>
+                                        <div className=" flex justify-between items-center   rounded-md" >
+                                            <div
+                                                onClick={() => handleAddToCart(variants?.[0]?.id)}
+
+                                                className=" cursor-pointer py-3 px-1">
+                                                <BiPlusMedical size={10} />
+                                            </div>
+                                            <div className="py-3 px-1 ">
+
+                                                {items?.quantity}
+                                            </div>
+                                            <div
+                                                onClick={handleDecrementToCart}
+                                                className="cursor-pointer py-3 px-1 ">
+                                                <ImMinus size={10} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    :
+                                    <button
+
+                                        onClick={() => handleAddToCart(variants?.[0]?.id)}
+                                        className='  border-2 border-yellow-400 rounded-lg text-red-500  hover:text-slate-50 text-xs font-bold hover:duration-500 duration-500  py-3 px-1 md:px-6 w-full    hover:bg-gradient-to-r from-yellow-400 to-red-600' > Add to Cart</button >
+                            }
+                        </div>
                     }
                 </div>
             </ div>
