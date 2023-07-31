474300a69409823b9d3b660890050f6bfb25ed35
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2023-07-31 23:06:54 +0600

    remove 3line button

--- src/Components/Sheard/Drawer/CartDrawer.js
+++ src/Components/Sheard/Drawer/CartDrawer.js
@@ -11,14 +11,7 @@ const CartDrawer = ({open,setOpen,children}) => {
     };
     return (
         <div>
-            <ButtonToolbar>
-                <IconButton className='!m-0' icon={<RiMenu3Line size={20} />}
 
-                            onClick={() => handleOpen('right')}>
-
-                </IconButton>
-
-            </ButtonToolbar>
 
             <Drawer  size={"xs"}   placement='right' open={open} onClose={() => setOpen(false)}>
                 <Drawer.Body className='p-0 h-screen  overflow-hidden overflow-y-auto'  >
