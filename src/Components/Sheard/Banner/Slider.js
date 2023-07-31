97cd00900203c52bb22ed0defcbf106d8cb23b03
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2023-07-31 23:21:12 +0600

    update banner

--- src/Components/Sheard/Banner/Slider.js
+++ src/Components/Sheard/Banner/Slider.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Carousel } from 'rsuite';
-// import { useHomeBannerMenuQuery } from "../../../api";
-// import { BannerSkeleton } from '../Skeletons/BannarSkeleton';
+import { useHomeBannerMenuQuery } from "../../../api";
+import { BannerSkeleton } from '../Skeletons/BannarSkeleton';
 
 
 
 const Slider = () => {
 
-  /*   const { data, loading } = useHomeBannerMenuQuery({
+     const { data, loading } = useHomeBannerMenuQuery({
 
         variables: {
             locale: "EN",
             channel: "default"
         }
-    }) */
+    })
 
 
     return (
@@ -29,23 +29,32 @@ const Slider = () => {
                                     src={slideImage?.collection?.backgroundImage?.url} alt="" />
 
                             </div>
-                        ))} */}
+                        ))}
+
+                        */}
 
             {/* </Carousel> */}
 
 
             <Carousel autoplay className='w-full h-full xl:rounded-xl lg:rounded-xl' >
-                <img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-neem_7CCP0DP.webp" alt=""  />
-                <img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-wholesale_1_Jc3OQqw.webp" alt=""  />
-                <img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-pre-order_INI3nSJ.webp" alt=""  />
-                <img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-noodles_i8cSXTy.webp" alt=""  />
-                <img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-feminine-products_vSQ8q39.webp" alt=""  />
-                <img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-powder_9KpPlSK.webp" alt=""  />
-        
 
+                {
+                    data?.menu?.items.map((slideImage, index) => (
+                        <img  className=" w-full" src={slideImage?.collection?.backgroundImage?.url} alt=""  />
+                    ))
+                }
             </Carousel>
         </div>
     );
 };
 
 export default Slider;
+
+
+{/*<img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-neem_7CCP0DP.webp" alt=""  />*/}
+{/*<img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-wholesale_1_Jc3OQqw.webp" alt=""  />*/}
+{/*<img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-pre-order_INI3nSJ.webp" alt=""  />*/}
+{/*<img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-noodles_i8cSXTy.webp" alt=""  />*/}
+{/*<img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-feminine-products_vSQ8q39.webp" alt=""  />*/}
+{/*<img src="https://shatkora.sgp1.cdn.digitaloceanspaces.com/media/discount/banner_offers/website-banner-powder_9KpPlSK.webp" alt=""  />*/}
+
