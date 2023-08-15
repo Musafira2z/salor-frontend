2412c35fe507984d69f9a9757c6084b509bc894c
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2023-08-15 14:50:57 +0600

    footer add

--- src/Dashboard/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard/Dashboard.js
@@ -6,18 +6,20 @@ import Footer from "./Footer";
 
 const Dashboard = () => {
     return (
-        <div className='h-screen md:flex-none flex flex-col justify-between' >
-            <NavigationBar />
-            <div className='container mx-auto' >
-                <div className=' flex justify-around gap-5' >
-                    <div className=' w-80 z-50 hidden xl:block lg:block md:block' >
-                        <DashboardSidebar />
-                    </div >
-                    <div className=' w-full xl:ml-64 lg:ml-56 md:ml-50 md:mx-5    mt-5' >
-                        <Outlet />
-                    </div >
-                </div >
-            </div >
+        <div className='h-screen flex flex-col justify-between' >
+           <div>
+               <NavigationBar />
+               <div className='container mx-auto ' >
+                   <div className=' flex justify-around gap-5' >
+                       <div className=' w-80 z-50 hidden xl:block lg:block md:block' >
+                           <DashboardSidebar />
+                       </div >
+                       <div className=' w-full xl:ml-64 lg:ml-56 md:ml-50 md:mx-5    mt-5' >
+                           <Outlet />
+                       </div >
+                   </div >
+               </div >
+           </div>
             <div className="mt-10 md:hidden ">
                 <Footer/>
             </div>
