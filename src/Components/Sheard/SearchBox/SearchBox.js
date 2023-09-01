4083abd9ab6eb2f6fe1cc04b6b454c6ba300b9d9
RnRukon <rukon.js@gmail.com>
2023-09-01 20:43:45 +0600

    placeholder content update in search field

--- src/Components/Sheard/SearchBox/SearchBox.js
+++ src/Components/Sheard/SearchBox/SearchBox.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-import {BiSearch} from 'react-icons/bi';
-import {useNavigate} from "react-router-dom";
+import { BiSearch } from 'react-icons/bi';
+import { useNavigate } from "react-router-dom";
 
 const SearchBox = () => {
-    const navigate=useNavigate()
-    const  [searchValue,setSearchValue]=useState('');
+    const navigate = useNavigate()
+    const [searchValue, setSearchValue] = useState('');
 
     const handleOnchange = (e) => {
         if (e.target.value) {
-           navigate(`/grocery?search=${e.target.value}`);
+            navigate(`/grocery?search=${e.target.value}`);
             setSearchValue(e.target.value)
         } else {
-          navigate('/');
+            navigate('/');
             setSearchValue('')
         }
     };
@@ -35,12 +35,12 @@ const SearchBox = () => {
                             e.stopPropagation();
                         }
                     }}
-                    className=" placeholder:text-slate-400  placeholder:text-base block bg-white w-full border border-slate-300 rounded-md rounded-r-none py-3 pl-24 pr-3 shadow-sm focus:outline-none focus:border-amber-500 focus:ring-amber-500 focus:ring-1 text-base " placeholder="Search your Product from hear" type="text" name="search" />
+                    className=" placeholder:text-slate-400  placeholder:text-base block bg-white w-full border border-slate-300 rounded-md rounded-r-none py-3 pl-24 pr-3 shadow-sm focus:outline-none focus:border-amber-500 focus:ring-amber-500 focus:ring-1 text-base " placeholder="Search your Product" type="text" name="search" />
 
 
             </label >
             <button
-                className=' bg-amber-500  flex gap-1 justify-center items-center w-32 text-slate-50 rounded-lg rounded-l-none text-base font-bold' ><BiSearch size={17}/>  Search</button >
+                className=' bg-amber-500  flex gap-1 justify-center items-center w-32 text-slate-50 rounded-lg rounded-l-none text-base font-bold' ><BiSearch size={17} />  Search</button >
         </div >
     );
 };
