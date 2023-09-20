b030854af09a7f161c145f92666126e9dd11e485
RnRukon <rukon.js@gmail.com>
2023-09-20 20:20:24 +0600

    cart position update  for sm

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -19,23 +19,26 @@ const Cart = () => {
 
 
     return (
-        <button >
-            <div onClick={() => setIsOpenCart(!isOpenCart)} className='  bg-amber-500 to-pink-600  w-auto h-auto rounded-lg rounded-r-none z-30  fixed top-1/2  right-0 flex flex-col justify-between cursor-pointer select-none min-w-[7rem]'>
-                <span className=' text-slate-50 text-lg px-3 py-4  text-center font-bold flex  justify-center items-center gap-2'>
+       <div className="fixed md:top-1/2  bottom-3 md:left-auto left-0 right-0 mx-5 md:mx-0">
+           <div className="w-full">
+               <button onClick={() => setIsOpenCart(!isOpenCart)} className="bg-amber-500 rounded-lg md:rounded-r-none z-30  select-none md:min-w-[7rem]  md:min-h-[7rem] md:w-[8rem] w-full h-14">
+                   <div  className='flex md:flex-col justify-between items-center cursor-pointer  '>
+                <span className=' text-slate-50 text-lg px-3 md:py-4  text-center font-bold flex  justify-center items-center gap-2'>
                     <BsFillBagCheckFill /> {checkoutData?.lines?.length || '00'} Items</span>
 
-                <span
-                    className=' bg-slate-50 text-slate-800 text-xs mx-3 mb-3 px-4 py-2 rounded-lg font-bold '>
+                       <span
+                           className=' bg-slate-50 text-slate-800 text-xs mx-3 md:mb-3 px-4 py-2 rounded-lg font-bold '>
                     R {checkoutData?.totalPrice?.gross?.amount}
                 </span>
-            </div>
+                   </div>
+                   <ShowAddToCartProduct
+                       isOpenCart={isOpenCart}
+                       setIsOpenCart={setIsOpenCart}
+                       checkoutData={checkoutData} />
+               </button>
 
-
-            <ShowAddToCartProduct
-                isOpenCart={isOpenCart}
-                setIsOpenCart={setIsOpenCart}
-                checkoutData={checkoutData} />
-        </button>
+           </div>
+       </div>
     );
 };
 
