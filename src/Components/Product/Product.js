3d93921a5bfe431110aac14e38f970c81f98ebe4
RnRukon <rukon.js@gmail.com>
2023-09-30 16:35:54 +0600

    update

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,22 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import BackButton from '../../Utility/Button/BackButton';
 import Cart from '../Cart/Cart';
-import { LanguageCodeEnum, useCheckoutAddProductLineMutation, useCheckoutByTokenQuery, useCheckoutLineUpdateMutation, useRemoveProductFromCheckoutMutation } from '../../api';
+import {
+    LanguageCodeEnum,
+    useCheckoutAddProductLineMutation,
+    useCheckoutByTokenQuery,
+    useCheckoutLineUpdateMutation,
+    useRemoveProductFromCheckoutMutation
+} from '../../api';
 import toast from "react-hot-toast";
-import { BiPlusMedical } from 'react-icons/bi';
-import { ImMinus } from 'react-icons/im';
+import {BiPlusMedical} from 'react-icons/bi';
+import {ImMinus} from 'react-icons/im';
 
-const Product = ({ data }) => {
+const Product = ({data}) => {
     const [media, setMedia] = useState('');
 
 
-    const [checkoutAddProductLine, { data: checkoutAddProduct }] = useCheckoutAddProductLineMutation();
-    const [decrement, { data: decrementData }] = useCheckoutLineUpdateMutation();
+    const [checkoutAddProductLine, {data: checkoutAddProduct}] = useCheckoutAddProductLineMutation();
+    const [decrement, {data: decrementData}] = useCheckoutLineUpdateMutation();
     const [RemoveProductFromCheckout] = useRemoveProductFromCheckoutMutation();
     const description = JSON.parse(data?.product?.description);
 
     const checkoutToken = JSON.parse(localStorage.getItem('checkoutToken'));
-    const { data: checkoutData } = useCheckoutByTokenQuery({
+    const {data: checkoutData} = useCheckoutByTokenQuery({
         variables: {
             checkoutToken: checkoutToken,
             locale: LanguageCodeEnum.En,
@@ -24,11 +30,7 @@ const Product = ({ data }) => {
     });
 
 
-
     const items = checkoutData?.checkout?.lines?.find(item => item?.variant?.id === data?.product?.variants[0]?.id);
-
-
-
 
 
     const handleAddToCart = async () => {
@@ -95,10 +97,10 @@ const Product = ({ data }) => {
     useEffect(() => {
 
         if (decrementData?.checkoutLinesUpdate?.checkout?.id) {
-            toast.success(`Quantity: ${items?.quantity || "00"}`, { id: 'checkout' })
+            toast.success(`Quantity: ${items?.quantity || "00"}`, {id: 'checkout'})
         }
         if (decrementData?.checkoutLinesUpdate?.errors?.[0]?.message) {
-            toast.error(decrementData?.checkoutLinesUpdate?.errors?.[0]?.message, { id: 'checkout' })
+            toast.error(decrementData?.checkoutLinesUpdate?.errors?.[0]?.message, {id: 'checkout'})
         }
 
 
@@ -115,12 +117,13 @@ const Product = ({ data }) => {
     return (
         <div>
             <div className='relative top-4'>
-                <BackButton />
+                <BackButton/>
             </div>
             <div className=' grid  lg:grid-cols-2 md:grid-cols-1 gap-10 '>
                 <div className=' bg-white '>
                     <div className=' flex justify-center items-center py-2'>
-                        <img className='md:h--96 h-52' src={media || data?.product?.media?.[0]?.url} alt="media" loading="lazy"/>
+                        <img className='md:h--96 h-52' src={media || data?.product?.media?.[0]?.url} alt="media"
+                             loading="lazy"/>
                     </div>
 
                     <div className='flex justify-center py-5 gap-2'>
@@ -141,7 +144,8 @@ const Product = ({ data }) => {
 
                         <div>
                             <h1 className=' text-black font-bold text-lg leading-normal'>{data?.product?.name}</h1>
-                            <p className='text-lg font-bold pt-2'>Available Quantity: {data?.product?.variants[0]?.quantityAvailable} </p>
+                            <p className='text-lg font-bold pt-2'>Available
+                                Quantity: {data?.product?.variants[0]?.quantityAvailable} </p>
 
                             <p className='text-lg '>{description?.blocks?.[0]?.data?.text}</p>
 
@@ -153,35 +157,36 @@ const Product = ({ data }) => {
                                     <p
                                         className=' text-red-500 text-lg mt-0 line-through'>
 
-                                        R {data?.product?.variants?.[0]?.pricing?.priceUndiscounted?.gross?.amount}</p >
+                                        R {data?.product?.variants?.[0]?.pricing?.priceUndiscounted?.gross?.amount}</p>
                                 }
-                                <p className='text-green-500 font-extrabold  text-2xl mt-0' >R {data?.product?.variants[0]?.pricing?.price?.gross?.amount}</p>
+                                <p className='text-green-500 font-extrabold  text-2xl mt-0'>R {data?.product?.variants[0]?.pricing?.price?.gross?.amount}</p>
                             </div>
                         </div>
                     </div>
 
                     <div className='flex md:justify-start justify-center md:gap-10 gap-2 mt-5'>
 
-                        <div >
+                        <div>
                             {
                                 items ?
 
-                                    <div className={`border-2 ${data?.product?.variants[0]?.quantityAvailable === items?.quantity ? "border-red-400 bg-red-400 text-white" : "border-amber-500 bg-amber-500 text-white"}  rounded   text-base font-semibold   py-1 px-4 md:px-6 w-full    `}>
-                                        <div className=" flex justify-between flex-row-reverse items-center   rounded" >
+                                    <div
+                                        className={`border-2  w-32 py-1 px-1   ${data?.product?.variants[0]?.quantityAvailable === items?.quantity ? "border-red-400 bg-red-400 text-white" : "border-amber-500 bg-amber-500 text-white"}  rounded   text-base font-semibold     `}>
+                                        <div
+                                            className=" flex justify-between flex-row-reverse items-center rounded">
                                             <button
                                                 disabled={data?.product?.variants[0]?.quantityAvailable === items?.quantity ? true : false}
                                                 onClick={handleAddToCart}
                                                 className=" cursor-pointer">
-                                                <BiPlusMedical size={15} />
+                                                <BiPlusMedical size={15}/>
                                             </button>
-                                            <div >
-
+                                            <div>
                                                 {items?.quantity}
                                             </div>
                                             <button
                                                 onClick={handleDecrementToCart}
                                                 className="cursor-pointer">
-                                                <ImMinus size={15} />
+                                                <ImMinus size={15}/>
                                             </button>
                                         </div>
                                     </div>
@@ -193,13 +198,15 @@ const Product = ({ data }) => {
                                         {data?.product?.variants[0]?.quantityAvailable === 0 ?
                                             <button
                                                 disabled
-                                                className='  border-2 border-red-500 rounded text-white bg-red-500   text-base font-semibold hover:duration-500 duration-500  py-1 px-4 md:px-6 w-full    '>Out Of Stock</button > :
+                                                className='  w-32 border-2 border-red-500 rounded text-white bg-red-500   text-base font-semibold hover:duration-500 duration-500  py-1 px-1     '>Out
+                                                Of Stock</button> :
 
-                                            <button onClick={handleAddToCart} className='addToCart border-2 border-amber-500 rounded text-amber-500 bg-white  text-base font-semibold hover:duration-500 duration-500  py-1 px-4 md:px-6 w-full   hover:border-amber-500 hover:bg-amber-500 hover:text-white' >
+                                            <button onClick={handleAddToCart}
+                                                    className=' w-32 addToCart border-2 border-amber-500 rounded text-amber-500 bg-white  text-base font-semibold hover:duration-500 duration-500  py-1   hover:border-amber-500 hover:bg-amber-500 hover:text-white'>
                                                 <div className='cartAnimation h-16 w-16 '>
                                                     <img src={data?.product?.media?.[0]?.url} alt="img" loading="lazy"/>
                                                 </div>
-                                                Add to cart</button >
+                                                Add to cart</button>
                                         }
                                     </div>
                             }
@@ -210,7 +217,7 @@ const Product = ({ data }) => {
                 </div>
             </div>
 
-            {checkoutData?.checkout?.lines?.length ? <Cart /> : null}
+            {checkoutData?.checkout?.lines?.length ? <Cart/> : null}
         </div>
     );
 };
--- src/Components/Sheard/ProductCard/ProductCard.js
+++ src/Components/Sheard/ProductCard/ProductCard.js
@@ -1,27 +1,36 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import './productCard.css';
-import { Link } from 'react-router-dom';
-import { LanguageCodeEnum, useCheckoutAddProductLineMutation, useCheckoutByTokenQuery, useCheckoutLineUpdateMutation, useRemoveProductFromCheckoutMutation } from '../../../api';
+import {Link} from 'react-router-dom';
+import {
+    LanguageCodeEnum,
+    useCheckoutAddProductLineMutation,
+    useCheckoutByTokenQuery,
+    useCheckoutLineUpdateMutation,
+    useRemoveProductFromCheckoutMutation
+} from '../../../api';
 
 import toast from 'react-hot-toast';
-import { BiPlusMedical } from 'react-icons/bi';
-import { ImMinus } from 'react-icons/im';
+import {BiPlusMedical} from 'react-icons/bi';
+import {ImMinus} from 'react-icons/im';
 import scrollToTop from "../../../Hooks/useSmoothScrolling";
 import LazyImgLoader from "../../LazyImgLoader/LazyImgLoader";
 
 
-const ProductCard = ({ data }) => {
+const ProductCard = ({data}) => {
 
-    const { thumbnail, name, slug, variants } = data?.node;
+    const {thumbnail, name, slug, variants} = data?.node;
 
-    const [checkoutAddProductLine, { data: checkoutAddProduct, checkoutAddProductLoading }] = useCheckoutAddProductLineMutation();
-    const [decrement, { data: decrementData, loading: decrementLoading }] = useCheckoutLineUpdateMutation();
+    const [checkoutAddProductLine, {
+        data: checkoutAddProduct,
+        checkoutAddProductLoading
+    }] = useCheckoutAddProductLineMutation();
+    const [decrement, {data: decrementData, loading: decrementLoading}] = useCheckoutLineUpdateMutation();
     const [RemoveProductFromCheckout] = useRemoveProductFromCheckoutMutation();
 
     const checkoutToken = JSON.parse(localStorage.getItem('checkoutToken'));
 
 
-    const { data: checkoutProducts } = useCheckoutByTokenQuery({
+    const {data: checkoutProducts} = useCheckoutByTokenQuery({
         variables: {
             checkoutToken: checkoutToken,
             locale: LanguageCodeEnum.En,
@@ -30,12 +39,7 @@ const ProductCard = ({ data }) => {
     const checkoutData = checkoutProducts?.checkout;
 
 
-
-
     const items = checkoutData?.lines?.find(item => item?.variant?.id === variants?.[0]?.id);
-
-
-
 
 
     const handleAddToCart = async (variantId) => {
@@ -50,7 +54,6 @@ const ProductCard = ({ data }) => {
     }
 
 
-
     const handleRemoveToCart = async () => {
         await RemoveProductFromCheckout({
             variables: {
@@ -60,8 +63,6 @@ const ProductCard = ({ data }) => {
             }
         })
     }
-
-
 
 
     const handleDecrementToCart = async () => {
@@ -83,17 +84,15 @@ const ProductCard = ({ data }) => {
     }
 
 
-
-
     useEffect(() => {
         if (decrementLoading) {
-            toast.loading('Loading...', { id: 'checkout' })
+            toast.loading('Loading...', {id: 'checkout'})
         }
         if (decrementData?.checkoutLinesUpdate?.errors?.[0]?.message) {
-            toast.error(decrementData?.checkoutLinesUpdate?.errors?.[0]?.message, { id: 'checkout' })
+            toast.error(decrementData?.checkoutLinesUpdate?.errors?.[0]?.message, {id: 'checkout'})
         }
         if (decrementData?.checkoutLinesUpdate?.checkout?.id) {
-            toast.success(`Quantity: ${items?.quantity || "00"}`, { id: 'checkout' })
+            toast.success(`Quantity: ${items?.quantity || "00"}`, {id: 'checkout'})
         }
 
     }, [
@@ -102,9 +101,6 @@ const ProductCard = ({ data }) => {
         decrementData?.checkoutLinesUpdate?.checkout?.id,
         decrementLoading
     ]);
-
-
-
 
 
     // error handling -------------------
@@ -136,37 +132,43 @@ const ProductCard = ({ data }) => {
 
     return (
 
-        <div className=" md:h-[17rem] h-[14rem]   lg:rounded-lg rounded-md   md:border-none border  border-amber-100  bg-white flex flex-col justify-between md:p-3 p-2">
+        <div
+            className=" md:h-[17rem] h-[14rem]   lg:rounded-lg rounded-md   md:border-none border  border-amber-100  bg-white flex flex-col justify-between md:p-3 p-2">
 
 
             <Link to={`/product-details/${slug}`}
                   onClick={scrollToTop}
-                className='hover:no-underline focus:no-underline hover:text-slate-700 focus:text-slate-700'
+                  className='hover:no-underline focus:no-underline hover:text-slate-700 focus:text-slate-700'
             >
 
 
-                <div className=' flex justify-center' >
+                <div className=' flex justify-center'>
                     {/*<img src={thumbnail?.url} alt={name} className="md:h-24 h-20 object-contain" loading={"lazy"}/>*/}
                     <LazyImgLoader
                         src={thumbnail?.url}
                         alt={name}
-                        style={{height:"5rem", width:"100%"} }
+                        style={{height: "5rem", width: "100%"}}
                     />
-                </div >
+                </div>
 
 
-
-                <div className='md:mt-5 sm:mt-3 mt-2 ' >
-                    <p className='truncate hover:text-clip' style={{ fontSize: '15px', color: 'rgb(13, 17, 54)', width: '100%', whiteSpace: 'nowrap', fontWeight: 'bold' }}>{name}</ p>
+                <div className='md:mt-5 sm:mt-3 mt-2 '>
+                    <p className='truncate hover:text-clip' style={{
+                        fontSize: '15px',
+                        color: 'rgb(13, 17, 54)',
+                        width: '100%',
+                        whiteSpace: 'nowrap',
+                        fontWeight: 'bold'
+                    }}>{name}</ p>
 
                     {/* <p className='text-md text-gray-500 font-bold'>Available Quantity: {data?.node?.variants?.[0]?.quantityAvailable}</p> */}
 
-                    <div >
+                    <div>
                         <p className=" md:text-sm text-xs">{data?.node?.variants?.[0]?.attributes?.[0]?.values?.[0]?.name}</p>
 
-                    </div >
+                    </div>
 
-                </div >
+                </div>
             </Link>
 
             <div className=' '>
@@ -174,52 +176,57 @@ const ProductCard = ({ data }) => {
                     {variants?.[0]?.pricing?.price?.gross?.amount !== variants?.[0]?.pricing?.priceUndiscounted?.gross?.amount &&
                         <del
                             className=' text-red-500 font-extrabold '
-                            style={{ fontSize: '15px', fontWeight: '700' }}>
+                            style={{fontSize: '15px', fontWeight: '700'}}>
 
-                            R {variants?.[0]?.pricing?.priceUndiscounted?.gross?.amount}</del >}
+                            R {variants?.[0]?.pricing?.priceUndiscounted?.gross?.amount}</del>}
                     <span
                         className=' text-green-500 font-extrabold mt-0 '
-                        style={{ fontSize: '15px', fontWeight: '700' }}>R {variants?.[0]?.pricing?.price?.gross?.amount}</span >
+                        style={{
+                            fontSize: '15px',
+                            fontWeight: '700'
+                        }}>R {variants?.[0]?.pricing?.price?.gross?.amount}</span>
                 </div>
 
                 {data?.node?.variants?.[0]?.quantityAvailable === 0 ?
                     <button
                         disabled
-                        className='  border-2 border-red-500 rounded-lg text-white bg-red-500   md:text-base text-sm  font-semibold hover:duration-500 duration-500  py-1 px-2 md:px-3 w-full    ' >
-                        Out of stock</button > :
+                        className='  border-2 border-red-500 rounded-lg text-white bg-red-500   md:text-base text-sm  font-semibold hover:duration-500 duration-500  py-1  w-full    '>
+                        Out of stock</button> :
 
                     <div>
                         {
                             items ?
 
-                                <div className={`border-2 ${data?.node?.variants?.[0]?.quantityAvailable === items?.quantity ? "border-red-400 bg-red-400 text-white" : "border-amber-500 bg-amber-500 text-white"}  rounded-lg   md:text-base text-sm font-semibold hover:duration-500 duration-500  py-1 px-2 md:px-6 w-full    `}>
-                                    <div className=" flex justify-between flex-row-reverse items-center   rounded-md" >
+                                <div
+                                    className={`border-2 ${data?.node?.variants?.[0]?.quantityAvailable === items?.quantity ? "border-red-400 bg-red-400 text-white" : "border-amber-500 bg-amber-500 text-white"}  rounded-lg   md:text-base text-sm font-semibold hover:duration-500 duration-500  py-1 px-2 md:px-6 w-full    `}>
+                                    <div className=" flex justify-between flex-row-reverse items-center   rounded-md">
                                         <button
                                             disabled={data?.node?.variants?.[0]?.quantityAvailable === items?.quantity ? true : false}
                                             onClick={() => handleAddToCart(variants?.[0]?.id)}
                                             className=" cursor-pointer">
-                                            <BiPlusMedical size={15} />
+                                            <BiPlusMedical size={15}/>
                                         </button>
-                                        <div >
+                                        <div>
 
                                             {items?.quantity}
                                         </div>
                                         <button
                                             onClick={handleDecrementToCart}
                                             className="cursor-pointer">
-                                            <ImMinus size={15} />
+                                            <ImMinus size={15}/>
                                         </button>
                                     </div>
                                 </div>
                                 :
                                 <button
                                     onClick={() => handleAddToCart(variants?.[0]?.id)}
-                                    className=' relative addToCart border-2 border-amber-500 rounded-lg text-amber-500 bg-white  md:text-base text-sm font-semibold hover:duration-500 duration-500  py-1  md:px-6 w-full  flex items-center justify-center gap-x-1 hover:border-amber-500 hover:bg-amber-500 hover:text-white' >Add to cart
+                                    className=' relative addToCart border-2 border-amber-500 rounded-lg text-amber-500 bg-white  md:text-base text-sm font-semibold hover:duration-500 duration-500  py-1  md:px-6 w-full  flex items-center justify-center gap-x-1 hover:border-amber-500 hover:bg-amber-500 hover:text-white'>Add
+                                    to cart
 
                                     <div className='cartAnimation h-16 w-16 '>
-                                        <img src={thumbnail?.url} alt={name} loading={"lazy"} />
+                                        <img src={thumbnail?.url} alt={name} loading={"lazy"}/>
                                     </div>
-                                </button >
+                                </button>
                         }
                     </div>
                 }
